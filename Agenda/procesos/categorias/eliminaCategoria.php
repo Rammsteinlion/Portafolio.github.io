a1e4e4e83ae637cd666eedce03950d3371cd4560
elkinmurillo <rastaw28@gmail.com>
2022-02-10 12:14:08 -0500

    make it better

--- Agenda/procesos/categorias/eliminaCategoria.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-require_once '../../clases/categorias.php';
-$idCategoria = $_POST['idCategoria'];
-$categorias = new Categorias();
-echo $categorias->eliminaCategoria($idCategoria);
--- Agenda/procesos/categorias/obtenerDatosCategoria.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-require_once '../../clases/categorias.php';
-$idCategoria = $_POST['idCategoria'];
-$categorias = new Categorias();
-
-//este arreglo lo convierta en una cadena json 
-echo json_encode($categorias->obtenerDatos($idCategoria));
